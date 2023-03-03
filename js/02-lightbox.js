141d068bc5f383648e98418cc6ad2f8d7aa560c8
Serhiy Slipchuk <slipchuks@ukr.net>
2023-03-03 15:28:26 +0200

    Complete Task 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+createGalleryItemsHTML(galleryItems);
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250});
+
+function createGalleryItemsHTML(galleryItems) {
+    const items = galleryItems.map(({ preview, original, description }) => {
+        const itemEl = document.createElement('a');
+        itemEl.classList.add('gallery__item');
+        itemEl.href = original
+
+        const imageEl = document.createElement('img');
+        imageEl.classList.add('gallery__image');
+        imageEl.src = preview;
+        imageEl.alt = description;
+        itemEl.appendChild(imageEl);
+        return itemEl;
+    });
+
+    const listEl = document.querySelector('.gallery');
+    listEl.append(...items);
+}
